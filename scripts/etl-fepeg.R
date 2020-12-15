c6e8d2beca0e37f17df0dd24274c19c6ee88075d
Rik Ferreira Alves <rikferreiraalves@gmail.com>
2020-12-15 13:55:40 -0300

    fepeg milestone

--- scripts/etl-fepeg.R
+++ scripts/etl-fepeg.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(survey)
 library(PNADcIBGE)
+library(RPostgreSQL)
 
-micro_pnadc <- get_pnadc(
+micro2019 <- get_pnadc(
     year = 2019,
     interview = 1,
     vars = c(
@@ -14,11 +15,10 @@ micro_pnadc <- get_pnadc(
     design = FALSE
 )
 
-micro2019 %>%
+design2019 <- micro2019 %>%
     mutate(
         S01012A = replace_na(S01012A, "99"),
         SITUACAO = ifelse(V1022 == "1", "Urbano", "Rural") %>% as_factor() %>% fct_relevel("Urbano", "Rural"),
-        
         DOMICILIO = paste0(UPA, V1008),
         ADEQUACAO = ifelse(
             S01010 == 1 & (S01012A == 1 | S01012A == 2) & S01013 == 1,
@@ -31,3 +31,20 @@ micro2019 %>%
             fct_relevel(c("Adequado", "Semi-adequado", "Inadequado"))
     ) %>%
     pnadc_design()
+
+svytable(~ UF + SITUACAO + ADEQUACAO + S01010 + S01012A + S01013, design2019) %>%
+    as_tibble() %>%
+    rename(
+        AGUA = S01010,
+        ESGOTO = S01012A,
+        LIXO = S01013
+    ) %>%
+    mutate(
+        AGUA = ifelse(AGUA == "1", "Adequado", "Inadequado"),
+        ESGOTO = ifelse(ESGOTO %in% c("1", "2"), "Adequado", "Inadequado"),
+        LIXO = ifelse(LIXO == "1", "Adequado", "Inadequado")
+    ) %>%
+    group_by(UF, SITUACAO, ADEQUACAO, AGUA, ESGOTO, LIXO) %>%
+    summarise(n = sum(n, na.rm = TRUE) %>% round()) %>%
+    ungroup() %>%
+    write_csv("../output/saneamento_ufs.csv")
